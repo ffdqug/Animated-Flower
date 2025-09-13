546029046b78e2c28d17bd9b28844bba7492049a
ffdqug <naksnskpakao@gmail.com>
2025-09-13 08:13:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+  document.getElementById("showFlowersBtn").addEventListener("click", function () {
+    document.querySelector(".flowers1").style.display = "block";
+    this.style.display = "none"; // Sembunyikan tombol setelah diklik
+  });
+
+
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
 };
-
-  document.getElementById("showFlowersBtn").addEventListener("click", function () {
-    document.querySelector(".flowers1").style.display = "block";
-    this.style.display = "none"; // Sembunyikan tombol setelah diklik
-  });
